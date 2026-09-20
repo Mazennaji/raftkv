b8244aee31dd769e7827fbf0aac5e780061e28ad
Mazennaji <mazen.naji.1@gmail.com>
2026-09-20 11:58:14 +0300

    feat: implement RequestVote and AppendEntries handling in Node

--- raft/node.go
+++ raft/node.go
@@ -58,3 +58,61 @@ func (n *Node) Term() uint64 {
 	defer n.mu.Unlock()
 	return n.currentTerm
 }
+
+func (n *Node) HandleRequestVote(args *RequestVoteArgs, reply *RequestVoteReply) error {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	if args.Term < n.currentTerm {
+		reply.Term = n.currentTerm
+		reply.VoteGranted = false
+		return nil
+	}
+
+	if args.Term > n.currentTerm {
+		n.currentTerm = args.Term
+		n.votedFor = 0
+		n.state = Follower
+	}
+
+	lastLogIndex, lastLogTerm := n.lastLogInfo()
+	logIsUpToDate := args.LastLogTerm > lastLogTerm ||
+		(args.LastLogTerm == lastLogTerm && args.LastLogIndex >= lastLogIndex)
+
+	if (n.votedFor == 0 || n.votedFor == args.CandidateID) && logIsUpToDate {
+		n.votedFor = args.CandidateID
+		reply.VoteGranted = true
+	} else {
+		reply.VoteGranted = false
+	}
+
+	reply.Term = n.currentTerm
+	return nil
+}
+
+func (n *Node) HandleAppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply) error {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	if args.Term < n.currentTerm {
+		reply.Term = n.currentTerm
+		reply.Success = false
+		return nil
+	}
+
+	n.currentTerm = args.Term
+	n.state = Follower
+	n.votedFor = 0
+
+	reply.Term = n.currentTerm
+	reply.Success = true
+	return nil
+}
+
+func (n *Node) lastLogInfo() (index uint64, term uint64) {
+	if len(n.log) == 0 {
+		return 0, 0
+	}
+	last := n.log[len(n.log)-1]
+	return last.Index, last.Term
+}
